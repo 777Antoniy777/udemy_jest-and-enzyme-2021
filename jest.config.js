4afd2a0dbaa479dfae9833cfff34b5571f177ecb
Anton Kuzmitsky <tat7106@gmail.com>
2021-08-20 12:50:04 +0600

    add initial config with Webpack 5, Typescript and Jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    rootDir: `./src`,
+    transform: {
+        "^.+\\.tsx?$": `ts-jest`,
+        "^.+\\.js?$": `babel-jest`,
+    },
+    testRegex: `.test.(js?|jsx?|tsx?)$`,
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|less)$": "identity-obj-proxy"
+    },
+    moduleFileExtensions: [`ts`, `tsx`, `js`, `jsx`, `json`, `node`],
+};
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require(`path`);
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+module.exports = {
+    mode: 'development',
+    entry: `./src/index.tsx`,
+    output: {
+        filename: `bundle.js`,
+        path: path.join(__dirname, `public`),
+        publicPath: "/public",
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, './public'),
+        },
+        port: 8080,
+        hot: isDev,
+        open: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: `babel-loader`,
+                },
+            },
+            {
+                test: /\.(tsx|ts)?$/,
+                loader: `ts-loader`
+            }
+        ],
+    },
+    resolve: {
+        extensions: [`.ts`, `.tsx`, `.js`, `json`]
+    },
+    devtool: `source-map`,
+};
